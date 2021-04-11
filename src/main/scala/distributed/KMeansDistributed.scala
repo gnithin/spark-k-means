8dee6c59c0e6baa4bb7eb6cc28aa61749de945f2
psx95 <sharma.prana@northeastern.edu>
2021-04-10 20:44:29 -0400

    Working implementation for  K-means

--- src/main/scala/distributed/KMeansDistributed.scala
+++ src/main/scala/distributed/KMeansDistributed.scala
@@ -1,7 +1,8 @@
 package distributed
 
 import org.apache.log4j.LogManager
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.avg
 
 object KMeansDistributed {
 
@@ -16,44 +17,52 @@ object KMeansDistributed {
       .appName("KMeans Sequential")
       .getOrCreate()
 
-    val textFile = spark.sparkContext.textFile(args(0))
-    val counts = textFile.flatMap(line => line.split(" "))
-      .map(word => (word, 1))
-      .reduceByKey(_ + _)
-
     logger.info("***************K Means Distributed*************");
-    val maxIterations = 100; // to prevent long programs - for convergence
+    val maxIterations = 10; // to prevent long programs - for convergence
     import spark.implicits._
     var centroidsList = spark.sparkContext.textFile(args(0) + "/centers.txt")
-      .map(point => (point.split(" ")(0).toFloat, point.split(" ")(1).toFloat))
+      .map(point => (point.split(",")(0).toDouble, point.split(",")(1).toDouble))
       .collect()
       .toList
 
     val samplePointsDS = spark.sparkContext.textFile(args(0) + "/sample-points.txt")
-      .map(point => (point.split(" ")(0).toFloat, point.split(" ")(1).toFloat))
+      .map(point => (point.split(",")(0).toDouble, point.split(",")(1).toDouble))
       .toDS()
 
     // currently run for max iterations
-    for(i <- 0 until maxIterations) {
+    var i = 0
+    var prevCentroids = centroidsList
+    for (i <- 0 until maxIterations) {
+      var updatedCenters = samplePointsDS.map(point => (bestCentroid(point, centroidsList), point._1, point._2))
+        .groupBy($"_1")
+        .agg(
+          avg($"_2").as("avg_x"),
+          avg($"_3").as("avg_y")
+        )
+      centroidsList = updatedCenters.select("avg_x", "avg_y").as[(Double, Double)].collect().toList
+      if (prevCentroids == centroidsList) {
+        println("CONVERGENCE REACHED")
+      }
     }
-
-
-    counts.saveAsTextFile(args(1))
+    if (i == maxIterations) {
+      println("MAX ITERATIONS REACHED")
+    }
+    spark.sparkContext.parallelize(centroidsList).toDF().coalesce(1).write.csv(args(1))
   }
 
   // returns the index of the best centroid
-  def bestCentroid(dataPoint: (Float, Float), centers: List[(Float, Float)]): Int = {
+  def bestCentroid(dataPoint: (Double, Double), centers: List[(Double, Double)]): (Double, Double) = {
     var bestIndex = 0
     var closestDistance = Double.PositiveInfinity
 
     for (i <- centers.indices) {
-      val distance = scala.math.sqrt(scala.math.pow((dataPoint._1 - centers(i)._1),2) + scala.math.pow((dataPoint._2 - centers(i)._2),2))
+      val distance = scala.math.sqrt(scala.math.pow((dataPoint._1 - centers(i)._1), 2) + scala.math.pow((dataPoint._2 - centers(i)._2), 2))
       if (distance < closestDistance) {
         closestDistance = distance
         bestIndex = i;
       }
     }
-    bestIndex
+    centers(bestIndex)
   }
 
 }

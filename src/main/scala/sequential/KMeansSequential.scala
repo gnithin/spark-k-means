0b45a5f5653a35903bf38f1b17eeee93403a17c1
Nithin <nithin.linkin@gmail.com>
2021-04-19 12:47:36 -0400

    [processing] Add logic to re-calculate centroid map

--- src/main/scala/sequential/KMeansSequential.scala
+++ src/main/scala/sequential/KMeansSequential.scala
@@ -11,6 +11,8 @@ import scala.collection.Map
 object KMeansSequential {
   val DATA_DIR = "data";
   val CONFIG_DIR = "configuration";
+  // TODO: Think about the correct entry
+  val MAX_ITERATIONS = 10
 
   def main(args: Array[String]): Unit = {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
@@ -49,26 +51,36 @@ object KMeansSequential {
     kValWithClustersPair.saveAsTextFile(args(1))
   }
 
-  def calculateDistance(point: (Double, Double), center: (Double, Double)): Double = {
-    // Square root of - (x2 - x1)^2 - (y2 - y1)^2
-    Math.pow(
-      Math.pow(point._1 - center._1, 2) + Math.pow(point._2 - center._2, 2),
-      0.5
-    )
+  def calculateDistance(
+                         point: Seq[Double],
+                         center: Seq[Double])
+  : Double = {
+    // Cosine similarity
+    // cos(theta) = A.B / |A|*|B|
+
+    val dotProduct = point.zip(center).
+      map(entry => entry._1 * entry._2).sum
+
+    val pointMagnitude = Math.pow(point.map(e => Math.pow(e, 2)).sum, 0.5)
+    val centerMagnitude = Math.pow(center.map(e => Math.pow(e, 2)).sum, 0.5)
+    val magnitude = pointMagnitude * centerMagnitude
+
+    dotProduct / magnitude
   }
 
-  def calculateSSE(kMeansMap: Map[(Double, Double), Vector[(Double, Double)]]): Double = {
-    kMeansMap.map(item => {
-      val center = item._1
-      val valuesList = item._2
+  // TODO: Fix this
+  //  def calculateSSE(kMeansMap: Map[(Double, Double), Vector[(Double, Double)]]): Double = {
+  //    kMeansMap.map(item => {
+  //      val center = item._1
+  //      val valuesList = item._2
+  //
+  //      valuesList.map(v =>
+  //        Math.pow(calculateDistance(v, center), 2)
+  //      ).sum
+  //    }).sum
+  //  }
 
-      valuesList.map(v =>
-        Math.pow(calculateDistance(v, center), 2)
-      ).sum
-    }).sum
-  }
-
-  def kMeans(k: Int, inputData: Map[String, Seq[Double]]): (Double, Map[(Double, Double), Vector[(Double, Double)]]) = {
+  def kMeans(k: Int, inputData: Map[String, Seq[Double]]): (Double, Map[Seq[Double], Vector[(String, Seq[Double])]]) = {
     // Get random centroids
     // NOTE: Sampling some entries without any repeats. This will be sufficient if inputData.length
     // is not super huge. Then again it is assumed that it can fit in memory, so we should be fine.
@@ -84,33 +96,36 @@ object KMeansSequential {
     centroids.foreach(println)
     println("*****")
 
-    var prevCentroids = Vector[(Double, Double)]()
+    var prevCentroids = Vector[Seq[Double]]()
 
-    var centroidMap: Map[(Double, Double), Vector[(Double, Double)]] = Map()
+    var centroidMap: Map[Seq[Double], Vector[(String, Seq[Double])]] = Map()
 
-    // TODO: Remove dummy return
-    (1.0, centroidMap)
+    var iterations = 0
 
-    /*
-    // Loop till convergence (centroids do not change)
-    while (!(centroids == prevCentroids)) {
+    // TODO: Fix the comparison for convergence
+    // Loop till convergence (centroids do not change or max-iterations reached)
+    while (!(centroids == prevCentroids) && iterations < MAX_ITERATIONS) {
+      iterations += 1
+
       // Reset the map
       centroidMap = Map()
 
       // Assign each input point to a centroid
-      inputData.foreach(point => {
+      inputData.foreach(document => {
+        val documentVector = document._2
+
         val minCentroidDistancePair = centroids.map(centroid => {
-          (centroid, calculateDistance(centroid, point))
+          (centroid, calculateDistance(centroid, documentVector))
         }).minBy(_._2)
         val minCentroid = minCentroidDistancePair._1
 
         if (centroidMap.contains(minCentroid)) {
           val clusterList = centroidMap(minCentroid)
-          val newClusterList = clusterList :+ point
+          val newClusterList = clusterList :+ document
           centroidMap += (minCentroid -> newClusterList)
 
         } else {
-          val newClusterList = Vector(point)
+          val newClusterList = Vector(document)
           centroidMap += (minCentroid -> newClusterList)
         }
       })
@@ -119,20 +134,20 @@ object KMeansSequential {
       println("----- Map")
       centroidMap.foreach(println)
 
-      // Recalculate centroids
-      prevCentroids = centroids
-      centroids = Vector[(Double, Double)]()
-
-      centroidMap.foreach(item => {
-        val pointsList = item._2
-        val pointSize = pointsList.length
-        val sumPoints = pointsList.reduce((l, r) => {
-          (l._1 + r._1, l._2 + r._2)
-        })
-
-        val avgPoints = (sumPoints._1 / pointSize, sumPoints._2 / pointSize)
-        centroids = centroids :+ avgPoints
-      })
+      //      // Recalculate centroids
+      //      prevCentroids = centroids
+      //      centroids = Vector[(Double, Double)]()
+      //
+      //      centroidMap.foreach(item => {
+      //        val pointsList = item._2
+      //        val pointSize = pointsList.length
+      //        val sumPoints = pointsList.reduce((l, r) => {
+      //          (l._1 + r._1, l._2 + r._2)
+      //        })
+      //
+      //        val avgPoints = (sumPoints._1 / pointSize, sumPoints._2 / pointSize)
+      //        centroids = centroids :+ avgPoints
+      //      })
 
       // TODO: Remove this at the end
       println("------ New centroid list")
@@ -140,7 +155,8 @@ object KMeansSequential {
       println("****** Iteration ends")
     }
 
-    (calculateSSE(centroidMap), centroidMap)
-     */
+    //    (calculateSSE(centroidMap), centroidMap)
+    // TODO: Remove dummy return
+    (1.0, centroidMap)
   }
 }
